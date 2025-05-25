1c18886b1aa2c495d07750deec84573f32793caa
yusufkhan792 <yusuf.patanwal792@gmail.com>
2025-05-25 21:28:25 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Yusuf Patanwala',
+    author_email= 'yusuf.patanwal792@@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
